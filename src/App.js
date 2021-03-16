7bf286dc4d892d57f422d78a8282e62c68899ea7
Brijbihari24 <55361395+Brijbihari24@users.noreply.github.com>
2021-03-17 00:50:12 +0530

    Udated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+// bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+//router-router
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+
+//toast
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+
+//Firebase
+import firebase from "firebase/app";
+import "firebase/auth";
+
+//Components
+import Home from "./Pages/Home";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import PageNotFound from "./Pages/PageNotFound";
+
+import { UserContext } from "./Contexts/UserContext";
+import Footer from "./Layout/Footer";
+import Header from "./Layout/Header";
+
+import firebaseConfig from "./Config/firebaseConfig";
+//init Firebase Authentication
+
+firebase.initializeApp(firebaseConfig);
+
+const App = () => {
+  const [user, setUser] = useState(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ToastContainer />
+      <UserContext.Provider value={{ user, setUser }}>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signin" component={SignIn} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="*" component={PageNotFound} />
+        </Switch>
+        <Footer />
+      </UserContext.Provider>
+    </Router>
   );
 }
 
